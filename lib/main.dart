e0ba3575afb3b3e6949e6bc1b8204fc7761a7eaf
Setianing Budi <setianingbudi2@gmail.com>
2025-01-07 19:15:36 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AttendanceApp());
+}
+
+class AttendanceApp extends StatefulWidget {
+  const AttendanceApp({super.key});
+
+  @override
+  State<AttendanceApp> createState() => _AttendanceAppState();
+}
+
+class _AttendanceAppState extends State<AttendanceApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
