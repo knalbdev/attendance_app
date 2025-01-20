c227856ec4124181f192cdd4798eb30d0ff138a1
Setianing Budi <setianingbudi2@gmail.com>
2025-01-20 11:03:20 +0700

    create poop-up notification

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,10 +1,113 @@
+import 'package:face_recognition/ui/attend/attendance_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return PopScope(
+      canPop: false,
+      // ignore: deprecated_member_use
+      onPopInvoked: (didPop) {
+        if (didPop) {
+          return;
+        }
+        _onWillPop(context);
+      },
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  margin: const EdgeInsets.all(10),
+                  child: Expanded(
+                    child: InkWell(
+                      highlightColor: Colors.transparent,
+                      splashColor: Colors.transparent,
+                      onTap: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => const AttendanceScreen()));
+                      },
+                      child: Column(
+                        children: [
+                          Image(
+                            image: AssetImage('/assets/images/ic_attend.png'),
+                            height: 100,
+                            width: 100,
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            "Attendance Report",
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 40),
+                
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<bool> _onWillPop(BuildContext context) async {
+    return (await showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (context) =>
+      AlertDialog(
+        title: Text(
+          "Information",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+            fontWeight: FontWeight.bold
+          ),
+        ),
+        content: Text(
+          "Do you want to exit?",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 16
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(false),
+            child: Text(
+              "No",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 14
+              ),
+            )
+          ),
+          TextButton(
+            onPressed: () => SystemNavigator.pop(),
+            child: Text(
+              "Yes",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 14
+              ),
+            )
+          )
+        ],
+      )
+      )
+    );
   }
 }
